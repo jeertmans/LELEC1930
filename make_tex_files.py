39a8262e222e8a8ca66ab57ca0f64b309e4b3711
Jérome Eertmans <jeertmans@icloud.com>
2022-04-08 16:11:34 +0200

    Auto make tex files

---
+++ make_tex_files.py
@@ -0,0 +1,39 @@
+import os
+
+if __name__ == "__main__":
+
+    files = os.listdir(".")
+
+    print(files)
+
+    prefix = "seance"
+
+    files = [file for file in files if file.startswith(prefix)]
+
+    for file in files:
+
+        number = file[len(prefix)]
+
+        exe_filename = f"TP_{number}.tex"
+        sol_filename = f"TP_{number}_solution.tex"
+
+        with open(file, "r") as f:
+
+            content = f.readlines()
+            content = [line for line in content if not r"\def\AvecSolutions{}" in line]
+
+        with open(exe_filename, "w") as f:
+
+            f.writelines(content)
+
+            print(exe_filename)
+
+        with open(sol_filename, "w") as f:
+
+            content.insert(1, r"\def\AvecSolutions{}")
+
+            f.writelines(content)
+
+            print(sol_filename)
+
+
